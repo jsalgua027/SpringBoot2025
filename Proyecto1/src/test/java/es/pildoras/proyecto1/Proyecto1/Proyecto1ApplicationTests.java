de853171d02b0f17038387d1e371d92ef4affda8
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2025-04-19 12:33:06 +0200

    primer proyecto spring

---
+++ Proyecto1/src/test/java/es/pildoras/proyecto1/Proyecto1/Proyecto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pildoras.proyecto1.Proyecto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
